f3253ced03a6bd4e4e284fc952da6bd715404454
Jim Kollevik <jim.kollevik@gmail.com>
2025-01-27 21:20:09 +0100

    custom domain
    change file src to custom domain

--- last-track.js
+++ last-track.js
@@ -1,6 +1,6 @@
 
     // URL to Json file in repo
-    const apiUrl = "https://jimkollevik.github.io/recent-tracks.json";
+    const apiUrl = "https://jimkollevik.com/recent-tracks.json";
 
     async function fetchRecentTracks() {
       const tracksDiv = document.getElementById("tracks");
